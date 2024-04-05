02e7ccd384bc54aa95962eed73b6a5cbf541258e
Jp Keys <jpmusik29@gmail.com>
2024-04-05 14:18:10 -0400

    Connecting the backend to the front end

--- studentLogin/src/App.jsx
+++ studentLogin/src/App.jsx
@@ -1,9 +1,21 @@
-import { useState, useEffect } from 'react'
+import { useState, useEffect } from 'react';
+import axios from "axios";
 
 function App() {
+  const [data, setData] = useState([])
+
+  const fetchAPI = async () =>{
+    const response = await axios.get("http://127.0.0.1:3000/students");
+    console.log(response.data.students);
+  }
+
+  useEffect(() => {
+    fetchAPI()
+  }, [])
+
   return(
     <div>
-      <h1>Flask React</h1>
+      <h1></h1>
     </div>
   )
 }
